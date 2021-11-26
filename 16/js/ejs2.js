8cffff9a507f13dee2d6d3938e60fb927da224c6
Donghyeon Baek <meister3090@gmail.com>
2021-11-26 18:10:46 +0900

    [Update] 11월 26일 실습

---
+++ 16/js/ejs2.js
@@ -0,0 +1,1 @@
+document.write("<div style = 'color: red; font-size: 24px;'> 두번째 외부 자바스크립트 파일</div>");
---
+++ 16/js/njs2.js
@@ -0,0 +1,3 @@
+document.write("njs2.js");
+document.write("<div style = 'color: blue; font-size: 20px;'> 두번째 외부 자바스크립트 파일</div>");
+document.write("<script src='js/njs3.js'></script>");
